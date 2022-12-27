1b806d8e96cbac18d5479c7392bf932448d01806
Tuan_Doan <doantuan247.hs@gmail.com>
2022-12-27 20:44:19 +0700

    HibernateConfig

---
+++ 2.Project_Connect_Use_Database/src/main/java/com/example2/Project_Non_Use_Database/config/HibernateConfig.java
@@ -0,0 +1,50 @@
+package com.example2.Project_Non_Use_Database.config;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class HibernateConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean(name = "dataSource")
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        return dataSource;
+    }
+
+    @Autowired
+    @Bean(name = "sessionFactory")
+    public SessionFactory getSessionFactory(DataSource dataSource) throws Exception {
+        Properties properties = new Properties();
+        properties.put("hibernate.dialect", env.getProperty("spring.jpa.properties.hibernate.dialect"));
+        properties.put("hibernate.show_sql", env.getProperty("spring.jpa.show_sql"));
+        properties.put("hbm2dd1.auto", env.getProperty("spring.jpa.hibernate.dd1-auto"));
+        properties.put("current_session_context_class", env.getProperty("spring.jpa.properties.hibernate.current_session_context_class"));
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+
+        //Package containi entity class
+        factoryBean.setPackagesToScan("com.example2.Project_Non_Use_Database.entities");
+        factoryBean.setDataSource(dataSource);
+        factoryBean.setHibernateProperties(properties);
+        factoryBean.afterPropertiesSet();
+
+        SessionFactory sf = factoryBean.getObject();
+        System.out.println("## getSessionFactory: " + sf);
+        return sf;
+    }
+}
