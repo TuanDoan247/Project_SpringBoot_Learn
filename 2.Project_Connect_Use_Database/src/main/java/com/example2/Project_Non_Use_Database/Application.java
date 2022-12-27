677afe2918318b433095f2921840ceec4aa52ea6
Tuan_Doan <doantuan247.hs@gmail.com>
2022-12-27 19:57:06 +0700

    Second Project

---
+++ 2.Project_Connect_Use_Database/src/main/java/com/example2/Project_Non_Use_Database/Application.java
@@ -0,0 +1,13 @@
+package com.example2.Project_Non_Use_Database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
