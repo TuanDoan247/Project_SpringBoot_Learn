999bdf2dc08568a959a304f54e229c22d5bd093d
Tuan_Doan <doantuan247.hs@gmail.com>
2022-12-28 20:50:54 +0700

    Delete Test Project

--- Test/src/main/java/com/example/Test/repository/EmployeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.Test.repository;
-
-import com.example.Test.entities.Employee;
-import org.springframework.data.repository.ListCrudRepository;
-
-public interface EmployeeRepository extends ListCrudRepository<Employee, Integer> {
-}
